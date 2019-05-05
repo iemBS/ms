1a0e3421eb680a50fdff35fe51325d6cf5afe6f0
sburnell <sburnell@gmx.de>
2019-05-04 23:46:39 -0700

    Add files via upload

---
+++ windowsShell/bat/execute SSIS package from command line (example).bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SQL Server\110\DTS\Binn\DTExec.exe"  /FILE "C:\Projects\CnO\ETL_Framework-VS2012\SSIS\ETLController.dtsx" /CONFIGFILE "C:\Projects\CnO\ETL_Framework-VS2012\SSIS\Connections.dtsConfig"  /CHECKPOINTING OFF  /REPORTING E  /SET "\Package.Variables[User::sJobName].Properties[Value]";AnoScorecardFY14 
+
+pause
