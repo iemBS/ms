95577681a62ab38852fe26d7477ee9af0017a8ca
sburnell <sburnell@gmx.de>
2019-05-05 22:19:11 -0700

    Create start remote cmdline session.ps1

---
+++ powershell/start remote cmdline session.ps1
@@ -0,0 +1,6 @@
+Start Remote PS Session
+Main Success Scenario:
+  1.	Start remote session
+        PS C:\Users\bbob> Enter-PSSession -ComputerName MyRemoteServer
+  2.	Display remote command prompt.
+        [MyRemoteServer]: PS C:\Users\bbob\Documents>
