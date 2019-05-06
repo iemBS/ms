0ca28d75adbd02a7b513f238969bf94c2c60575d
sburnell <sburnell@gmx.de>
2019-05-05 22:20:49 -0700

    Create exit remote PS session.ps1

---
+++ powershell/exit remote PS session.ps1
@@ -0,0 +1,6 @@
+Exit Remote PS Session
+Main Success Scenario:
+  1.	Exit remote session
+        [MyRemoteServer]: PS C:\> Exit-PSSession
+  2.	Display local command prompt
+        PS C:\Users\bbob>
