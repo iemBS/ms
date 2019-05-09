a5a825388a9489b8e88ec5600ffea22a0553ce82
sburnell <sburnell@gmx.de>
2019-05-09 12:09:32 -0700

    Update get single value from column.ps1
    clarified

--- powershell/get single value from column.ps1
+++ powershell/get single value from column.ps1
@@ -1,1 +1,6 @@
-$myColumn.myProperty
+Get Single Value from a Column
+Preconditions: 
+  - Column variable is named "myColumn"
+  - Property in that column is named "myProperty"
+Main Success Scenario
+  1. $myColumn.myProperty
