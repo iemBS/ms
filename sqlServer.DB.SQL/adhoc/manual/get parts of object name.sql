33a29c52c65db6a7089cf73365fed3307c7c23a0
sburnell <1821834+sburnell@users.noreply.github.com>
2019-08-06 14:56:43 -0700

    Create get parts of object name.sql

---
+++ sqlServer.DB.SQL/adhoc/manual/get parts of object name.sql
@@ -0,0 +1,5 @@
+SELECT PARSENAME('AdventureWorksPDW2012.dbo.DimCustomer', 1) AS 'Object Name';  
+SELECT PARSENAME('AdventureWorksPDW2012.dbo.DimCustomer', 2) AS 'Schema Name';  
+SELECT PARSENAME('AdventureWorksPDW2012.dbo.DimCustomer', 3) AS 'Database Name';  
+SELECT PARSENAME('AdventureWorksPDW2012.dbo.DimCustomer', 4) AS 'Server Name';  
+GO  
