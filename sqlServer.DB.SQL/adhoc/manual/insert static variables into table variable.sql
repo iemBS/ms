9877daf6ad9db76ab3e290c5a0fb0f0304794ee3
sburnell <1821834+sburnell@users.noreply.github.com>
2019-08-08 15:16:08 -0700

    Create insert static variables into table variable.sql

---
+++ sqlServer.DB.SQL/adhoc/manual/insert static variables into table variable.sql
@@ -0,0 +1,14 @@
+
+Declare @xyz table(
+abc varchar(10),
+pqr varchar(10)
+)
+
+insert into @xyz(abc, pqr)
+select abc, pqr
+from (VALUES
+    ('a1', 'p1'),
+    ('a2', 'p2'),
+    ('a3', 'p3')
+)res(abc, pqr)
+
