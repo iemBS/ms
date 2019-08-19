af266dc6793c2cc900637f27572e685287987efd
sburnell <1821834+sburnell@users.noreply.github.com>
2019-08-19 11:51:23 -0700

    Update do action on all tables in DB.sql
    note that sp_msforeachtable does not work on views

--- sqlServer.DB.SQL/adhoc/manual/do action on all tables in DB.sql
+++ sqlServer.DB.SQL/adhoc/manual/do action on all tables in DB.sql
@@ -1,3 +1,7 @@
 Do Action on All Tables in DB
+Note:
+  -Below query confirms "sp_msforeachtable" does not include views:
+    exec sp_MSforeachtable 'select TABLE_TYPE from INFORMATION_SCHEMA.TABLES  where TABLE_TYPE = ''VIEW'' AND QUOTENAME(TABLE_SCHEMA) + ''.'' + QUOTENAME(TABLE_NAME) = ''?'''
+
 Main Success Scenario:
   1. sp_msforeachtable 'print ''?'''
